731bb3f611144ccd2e98b51621c8daeda09296a9
UdithaIshan <uditha.ishan34@gmail.com>
2020-12-07 08:33:23 +0530

    testing

--- cli.js
+++ cli.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
+const {exec} = require("child_process");
+const add = require('./index');
 
-console.log(process.argv);
+exec("git status", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
