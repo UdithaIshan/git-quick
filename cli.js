534e82df42c1d20b7d76322ad5e44ae3f0b6920a
UdithaIshan <uditha.ishan34@gmail.com>
2020-12-09 07:03:48 +0530

    updates

--- cli.js
+++ cli.js
@@ -1,18 +1,13 @@
 #!/usr/bin/env node
 const {exec} = require('child_process');
-const add = require('./index');
-
+const cmdArray = process.argv;
+console.log(cmdArray);
 // const cmd = process.argv.slice(2).map( function(arg){
 //     return "'" + arg.replace(/'/g, "'\\''") + "'";
 //   }).join(' ');
 // console.log(cmd)
 
-const cmdArray = process.argv;
-console.log(cmdArray)
-
-if(cmdArray.length == 4) {
-    cmd = process.argv.slice(2).join(' ')
-    console.log(cmd);
+const cmdExec = (cmd) => {
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
@@ -24,23 +19,39 @@ if(cmdArray.length == 4) {
         }
         console.log(`stdout: ${stdout}`);
     });
-    
-}
-else if(cmdArray.length == 5) {
-    cmd = process.argv.slice(2, 4).join(' ');
-    msg = process.argv.slice(4).join(' ');
-    cmd = `${cmd} "${msg}"`;
-    console.log(cmd);
-    // exec(cmd, (error, stdout, stderr) => {
-    //     if (error) {
-    //         console.log(`error: ${error.message}`);
-    //         return;
-    //     }
-    //     if (stderr) {
-    //         console.log(`stderr: ${stderr}`);
-    //         return;
-    //     }
-    //     console.log(`stdout: ${stdout}`);
-    // });
 }
 
+// if(cmdArray.length == 4) {
+//     cmd = process.argv.slice(2).join(' ')
+//     console.log(cmd);
+//     exec(cmd, (error, stdout, stderr) => {
+//         if (error) {
+//             console.log(`error: ${error.message}`);
+//             return;
+//         }
+//         if (stderr) {
+//             console.log(`stderr: ${stderr}`);
+//             return;
+//         }
+//         console.log(`stdout: ${stdout}`);
+//     });
+    
+// }
+
+switch (process.argv[2]) {
+    case "i":
+        cmdExec('git init');
+        break;
+    case "a":
+        cmdExec('git add');
+        break;
+    case "c":
+        msg = process.argv.slice(3).join(' ')
+        console.log(msg);
+        // cmdExec('git init');
+        break;
+    case "p":
+
+    default:
+        break;
+}
