13d3a8e6538706619b3677d816b6cf0e43d13d05
UdithaIshan <uditha.ishan34@gmail.com>
2020-12-10 07:59:43 +0530

    updates

--- cli.js
+++ cli.js
@@ -17,39 +17,29 @@ const cmdExec = (cmd) => {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        console.log(`stdout: ${stdout}`);
+        // console.log(`stdout: ${stdout}`);
     });
 }
 
-// if(cmdArray.length == 4) {
-//     cmd = process.argv.slice(2).join(' ')
-//     console.log(cmd);
-//     exec(cmd, (error, stdout, stderr) => {
-//         if (error) {
-//             console.log(`error: ${error.message}`);
-//             return;
-//         }
-//         if (stderr) {
-//             console.log(`stderr: ${stderr}`);
-//             return;
-//         }
-//         console.log(`stdout: ${stdout}`);
-//     });
-    
-// }
+const quickPush = (branch, msg) => {
+    cmdExec('git add .');
+    cmdExec(`git commit -m "${msg}"`);
+    cmdExec(`git push origin ${branch}`);
+}
 
 switch (process.argv[2]) {
     case "i":
         cmdExec('git init');
         break;
     case "a":
-        cmdExec('git add .');
+        cmdExec(`git add ${process.argv.slice(3).join(' ')}`);
         break;
     case "c":
         cmdExec(`git commit -m "${process.argv.slice(3).join(' ')}"`);
         break;
     case "p":
-
+        process.argv[4] != null?quickPush(process.argv.slice(3,4).join(' '), process.argv.slice(4).join(' ')):cmdExec(`git push origin ${process.argv.slice(3).join(' ')}"`);
+        break;
     default:
         break;
 }
